4682df65991a1b4a2f7f1cd5abf801c292f2613d
zohar <zoharsim2000@gmail.com>
2026-03-10 13:02:57 +0200

    config web app

--- ToDoListRN/config.js
+++ ToDoListRN/config.js
@@ -1,5 +1,5 @@
 // Local development
-// export const API_URL = "http://10.0.2.2:5000";
+//export const API_URL = "http://10.0.2.2:5000";
 
 // Production (Render)
 export const API_URL = "https://final-todo-app-atj3.onrender.com";
