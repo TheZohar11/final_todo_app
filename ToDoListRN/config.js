3f984ccd5cac130e43389272bea7e0e13269fa9f
zohar <zoharsim2000@gmail.com>
2026-03-10 12:32:22 +0200

    local/render url

--- ToDoListRN/config.js
+++ ToDoListRN/config.js
@@ -1,3 +1,5 @@
-// Change this to your Render URL after deployment
-// Example: https://your-app-name.onrender.com
-export const API_URL = "http://10.0.2.2:5000";
+// Local development
+// export const API_URL = "http://10.0.2.2:5000";
+
+// Production (Render)
+export const API_URL = "https://final-todo-app-atj3.onrender.com";
