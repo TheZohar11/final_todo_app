cea440cbac71057336f1e395747d80ed80c429bc
zohar <zoharsim2000@gmail.com>
2026-09-11 14:14:57 +0300

    register screen v1

--- todoReact/src/pages/Register/Register.jsx
+++ todoReact/src/pages/Register/Register.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Landing from "../Landing/Landing";
+import Input from "../../components/Input/Input";
+import TextLarge from "../../components/TextLarge/TextLarge";
+import "./Register.css";
+import Button from "../../components/Button/Button";
 
 export default function Register() {
   return (
-    <>
+    <div className="container">
       <p>to be registerd</p>
-      <Link to="/Landing">Landing</Link>
-    </>
+      <TextLarge text="enter your email" />
+      <Input placeholder="youremail@gmail.com" type="email" />
+      <TextLarge text="enter your password" />
+      <Input placeholder="password.." type="password" />
+      <TextLarge text="verify password" />
+      <Input placeholder="password.." type="password" />
+      <Button text="Register" />
+    </div>
   );
 }
